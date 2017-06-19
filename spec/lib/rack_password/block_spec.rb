e8243e490c341175330ff4f7b2c49475e625643c
Bartosz Pranczke <bartosz.pranczke@netguru.pl>
2017-06-19 14:38:42 +0200

    Add spec

--- spec/lib/rack_password/block_spec.rb
+++ spec/lib/rack_password/block_spec.rb
@@ -13,6 +13,14 @@ module RackPassword
       it "return html" do
         expect(block.success_rack_response[2][0]).to include("password")
       end
+
+      it "fills in application name if used as Rails middleware" do
+        app_name = 'TestAppName'
+        class Rails; end
+        allow(Rails).to receive_message_chain(:application, :class, :parent_name) { app_name }
+        
+        expect(block.success_rack_response[2][0]).to include(app_name)
+      end
     end
   end
 end
